4d8eb0bfdd9bc867d14e6a1411ad8d3ac96a6e79
MonRaes <labs@labtec.local>
2023-08-26 11:17:10 -0300

    Desenvolvendo a funcao Intent

---
+++ app/src/main/java/sara/silva46/gerenciamento_intent/Tela2Activity.kt
@@ -0,0 +1,11 @@
+package sara.silva46.gerenciamento_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+    }
+}
